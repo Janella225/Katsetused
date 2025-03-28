4596ae43d17e282ff98b8d1138923f0bcbeb5ef4
Janella225 <133005244+Janella225@users.noreply.github.com>
2025-03-28 10:51:32 +0200

    Add files via upload

---
+++ kujund4.2.py
@@ -0,0 +1,46 @@
+# Minu nimi
+
+import turtle
+
+aken = turtle.Screen()
+aken.bgcolor("PaleTurquoise")
+
+turtle.speed(2)
+turtle.showturtle()
+turtle.shape("turtle")
+turtle.pensize(5)
+
+
+
+
+for i in range(5):
+    turtle.goto(0, -0)
+    turtle.left(72)
+    turtle.forward(-50)
+    turtle.pencolor("OrangeRed")
+
+    for i in range(4):
+        turtle.forward(100)
+        turtle.right(90)
+        turtle.pencolor("DeepPink")
+        
+        
+
+turtle.penup()
+turtle.goto(-300, 100)
+        
+
+
+turtle.done()
+    
+
+
+
+
+
+
+
+
+
+
+turtle.done()
